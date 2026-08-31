d99f22faf7be76862e8158e1abcdbcf245eed73b
JonasLillebo <lillebohaugen@gmail.com>
2026-08-31 23:53:50 +0200

    Update dependencies in go.mod and go.sum; implement database migration in tideauth.go

--- tideauth.go
+++ tideauth.go
@@ -2,11 +2,16 @@ package tideauth
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/lib/pq"
 	"github.com/threetides/tideauth/internal/auth"
 )
 
@@ -40,14 +45,25 @@ func New(cfg Config) (*Auth, error) {
 	return &Auth{db: db, cfg: cfg}, nil
 }
 
+func (a *Auth) Migrate(ctx context.Context) error {
+	db, err := sql.Open("postgres", a.cfg.DatabaseURL)
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	m, err := migrate.NewWithDatabaseInstance(
+		"/internal/migrations",
+		"postgres", driver)
+	m.Up()
+
+	if err != nil {
+		return fmt.Errorf("Error running migrations: %e", err)
+	}
+
+	log.Println("Migrations completed")
+	return nil
+}
+
 func (a *Auth) Routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /google/sign-in", auth.GoogleSignIn())
 	mux.HandleFunc("POST /sign-out", auth.SignOut())
 	return mux
-}
-
-func (a *Auth) Migrate(ctx context.Context) error {
-	log.Println("Migrations completed")
-	return nil
 }
