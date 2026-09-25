e5f289bee971c882a030be436eb0ccbe2a4ce9a5
JonasLillebo <lillebohaugen@gmail.com>
2026-09-26 00:44:21 +0200

    debug

--- tideauth.go
+++ tideauth.go
@@ -30,6 +30,9 @@ func (a *Auth) Migrate() error {
 		}
 	}()
 
+	fmt.Println(a.Config.DBurl)
+	fmt.Println(db)
+
 	if err := goose.SetDialect("postgres"); err != nil {
 		return fmt.Errorf("error setting dialect: %w", err)
 	}
