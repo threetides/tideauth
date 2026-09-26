f5c641c7874f5baad69c8dd03353c08caed70cae
JonasLillebo <lillebohaugen@gmail.com>
2026-09-26 15:34:17 +0200

    email, name, and password validation

--- go.mod
+++ go.mod
@@ -3,14 +3,17 @@ module github.com/threetides/tideauth
 go 1.27.0
 
 require (
+	github.com/AfterShip/email-verifier v1.5.0
 	github.com/briandowns/spinner v1.23.2
 	github.com/golang-migrate/migrate/v4 v4.20.1
 	github.com/jackc/pgx/v5 v5.11.0
+	github.com/wagslane/go-password-validator v0.3.0
 )
 
 require (
 	github.com/docker/go-connections v0.8.1 // indirect
 	github.com/fatih/color v1.7.0 // indirect
+	github.com/hbollon/go-edlib v1.7.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
@@ -18,11 +21,11 @@ require (
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/moby/moby/client v0.5.1 // indirect
-	github.com/stretchr/testify v1.12.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.71.0 // indirect
 	go.opentelemetry.io/otel/trace v1.46.0 // indirect
+	golang.org/x/net v0.58.0 // indirect
 	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
-	golang.org/x/term v0.44.0 // indirect
+	golang.org/x/term v0.45.0 // indirect
 	golang.org/x/text v0.41.0 // indirect
 )
--- internal/auth/handlers.go
+++ internal/auth/handlers.go
@@ -2,11 +2,15 @@ package auth
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"strings"
 
+	emailverifier "github.com/AfterShip/email-verifier"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/threetides/tideauth/internal/httpx"
+	passwordvalidator "github.com/wagslane/go-password-validator"
 )
 
 type Register struct {
@@ -14,6 +18,8 @@ type Register struct {
 	Name     string `json:"name"`
 	Password string `json:"password"`
 }
+
+var verifier = emailverifier.NewVerifier()
 
 func RegisterHandler(db *pgxpool.Pool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -32,12 +38,39 @@ func RegisterHandler(db *pgxpool.Pool) http.HandlerFunc {
 
 		if email == "" {
 			fieldErrors = append(fieldErrors, httpx.FieldError{Field: "email", Error: "email is required"})
+		} else {
+			if len(email) > 254 {
+				fieldErrors = append(fieldErrors, httpx.FieldError{Field: "email", Error: "email cannot contain more than 254 characters"})
+			}
+
+			res, err := verifier.Verify(email)
+			if err != nil {
+				log.Println("verify email address failed: ", err)
+				httpx.WriteJSON(w, http.StatusInternalServerError, "internal server error", nil)
+				return
+			}
+			if !res.Syntax.Valid {
+				fieldErrors = append(fieldErrors, httpx.FieldError{Field: "email", Error: "email is invalid"})
+				return
+			}
 		}
+
 		if name == "" {
 			fieldErrors = append(fieldErrors, httpx.FieldError{Field: "name", Error: "name is required"})
+		} else {
+			if len(email) > 100 {
+				fieldErrors = append(fieldErrors, httpx.FieldError{Field: "name", Error: "name cannot contain more than 100 characters"})
+			}
 		}
+
 		if password == "" {
 			fieldErrors = append(fieldErrors, httpx.FieldError{Field: "password", Error: "password is required"})
+		} else {
+			const minEntropyBits = 60
+			err := passwordvalidator.Validate(password, minEntropyBits)
+			if err != nil {
+				fieldErrors = append(fieldErrors, httpx.FieldError{Field: "password", Error: fmt.Sprintf("password is too weak, strenght: %f", passwordvalidator.GetEntropy(password))})
+			}
 		}
 
 		if len(fieldErrors) > 0 {
